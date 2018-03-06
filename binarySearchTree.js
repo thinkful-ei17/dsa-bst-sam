cf2b624f16d2d44b022b58f68120059e4ced01e4
Sam Gould <sjg326@gmail.com>
2018-03-06 08:50:11 -0800

    built functionality for BST insert method

---
+++ binarySearchTree.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+
+    else if (key >= this.key) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+}
